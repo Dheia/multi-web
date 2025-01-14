105b5baf1057bc6785ef52545f0ac2ead39b982d
Dheia <kddd90@gmail.com>
2025-01-14 16:51:24 +0300

    Delete assets directory

--- assets/common/js/SohanCustom.js
+++
@@ -1,9 +0,0 @@
-class SohanCustom{
-    static load_topbar_cart_nad_wishlist()
-    {
-        $('#track-icon-list').load(location.href + " #track-icon-list");
-        $('#track-icon-wishlist').load(location.href + " #track-icon-wishlist");
-        $('.track-icon-list').load(location.href + " .track-icon-list");
-        $('#compare_li').load(location.href + " #compare_li");
-    }
-}
